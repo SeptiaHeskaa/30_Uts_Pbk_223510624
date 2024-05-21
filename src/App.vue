02a7c0ab8dad89c851cbf76d28da9ba181e4a41c
septiaheskaa <heskaseptia@gmail.com>
2024-05-21 23:40:36 +0700

    tugas 11

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,26 @@
 <template>
   <div id="app">
-    <h1>My Activity</h1>
-    <div class="input-container">
-      <input type="text" v-model="newActivity" placeholder="New Task">
-      <button @click="addActivity">Add</button>
-    </div>
-
-    <ul v-if="!showingPosts">
-      <li v-for="(activity, index) in filteredActivities" :key="index" class="activity-item">
-        <span v-if="index !== editingIndex" :class="{ completed: activity.completed }">{{ activity.name }}</span>
-        <input v-else type="text" v-model="editedActivityName" @keyup.enter="saveEdit" @keyup.esc="cancelEdit">
-        <div>
-          <button @click="toggleCompletion(index)" class="action-button">{{ activity.completed ? 'Batal Checklist' : 'Checklist' }}</button>
-          <button @click="editActivity(index)" class="action-button">{{ index === editingIndex ? 'Simpan' : 'Edit' }}</button>
-          <button @click="removeActivity(index)" class="action-button">Remove</button>
-        </div>
-      </li>
-    </ul>
-
-    <div class="filter-container" v-if="!showingPosts">
-      <input type="checkbox" id="showOnlyPending" v-model="showOnlyPending">
-      <label for="showOnlyPending">Undone Task</label>
-    </div>
-
-    <button @click="showPostsView" v-if="!showingPosts">Show Posts</button>
-
-    <div v-if="showingPosts">
-      <h1>Postingan</h1>
-      <select v-model="selectedUser">
-        <option value="">Select user</option>
-        <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
-      </select>
-      <ul v-if="selectedUser">
-        <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
-      </ul>
-    </div>
-    
-    <button @click="showTodosView" v-if="showingPosts">Activity</button>
+    <button @click="toggleView">{{ showingPosts ? 'Activity' : 'Show Posts' }}</button>
+    <component :is="currentView"></component>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      newActivity: '',
-      editedActivityName: '',
-      editingIndex: -1,
-      activities: [],
-      showOnlyPending: false,
-      showingPosts: false,
-      selectedUser: null,
-      users: [],
-      posts: []
-    };
-  },
-  methods: {
-    addActivity() {
-      if (this.newActivity.trim() !== '') {
-        this.activities.push({ name: this.newActivity, completed: false });
-        this.newActivity = '';
-      }
-    },
-    removeActivity(index) {
-      this.activities.splice(index, 1);
-    },
-    toggleCompletion(index) {
-      this.activities[index].completed = !this.activities[index].completed;
-    },
-    editActivity(index) {
-      this.editedActivityName = this.activities[index].name;
-      this.editingIndex = index;
-    },
-    saveEdit() {
-      if (this.editedActivityName.trim() !== '') {
-        this.activities[this.editingIndex].name = this.editedActivityName;
-        this.cancelEdit();
-      }
-    },
-    cancelEdit() {
-      this.editedActivityName = '';
-      this.editingIndex = -1;
-    },
-    async fetchUsers() {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const data = await response.json();
-        this.users = data;
-      } catch (error) {
-        console.error('Error Fetching Users:', error);
-      }
-    },
-    async fetchPosts() {
-      if (!this.selectedUser) return;
-      try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${this.selectedUser}`);
-        const data = await response.json();
-        this.posts = data;
-      } catch (error) {
-        console.error('Error Fetching Posts:', error);
-      }
-    },
-    showPostsView() {
-      this.showingPosts = true;
-      this.fetchUsers();
-    },
-    showTodosView() {
-      this.showingPosts = false;
-    }
-  },
-  watch: {
-    selectedUser() {
-      this.fetchPosts();
-    }
-  },
-  computed: {
-    filteredActivities() {
-      if (this.showOnlyPending) {
-        return this.activities.filter(activity => !activity.completed);
-      }
-      return this.activities;
-    }
-  }
-};
+<script setup>
+import { ref, computed } from 'vue'
+import ActivityList from './components/ActivityList.vue'
+import PostList from './components/PostList.vue'
+
+const showingPosts = ref(false)
+
+const toggleView = () => {
+  showingPosts.value = !showingPosts.value
+}
+
+const currentView = computed(() => showingPosts.value ? PostList : ActivityList)
 </script>
 
 <style scoped>
+/* your styles here */
 html, body {
   height: 100%;
   margin: 0;
